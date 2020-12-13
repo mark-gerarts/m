767e7521fe4f7b6b658196a504fe18bcd2a8870c
Mark Gerarts <mark.gerarts@gmail.com>
2020-12-13 18:40:29 +0100

    Explain how to enable qwerty-fr

--- nixos/xserver/default.nix
+++ nixos/xserver/default.nix
@@ -7,7 +7,6 @@
     displayManager = {
       sddm.enable = true;
       defaultSession = "plasma5";
-      sessionCommands = "setxkbmap -layout qwerty-fr";
     };
 
     desktopManager.plasma5 = {
@@ -16,6 +15,17 @@
 
     layout = "qwerty-fr";
 
+    # I haven't figured out a declarative or nice way to get qwerty-fr working
+    # in plasma. The following allows the layout to be set with
+    #
+    # `setxkbmap -layout qwerty-fr`.
+    #
+    # Which I have saved to a shell script and configured KDE to run it on
+    # startup (NOT session startup, that won't work) in the autostart settings.
+    #
+    # I'd rather be able to actually install the layout and set it in the
+    # keyboard options, or directly here in the config. But hey, I spend enough
+    # time on just getting it to work already.
     extraLayouts = {
       qwerty-fr = {
         description = "QWERTY with French symbols and diacritics http://marin.jb.free.fr/qwerty-fr/";
