14dd70fd941a6c3adeeb2d9cedebf2e8369c5c74
Mark Gerarts <mark.gerarts@gmail.com>
2020-12-18 17:07:19 +0100

    Add smiley prompt

--- nix/bash.nix
+++ nix/bash.nix
@@ -3,6 +3,7 @@
 {
   programs.bash = {
     enable = true;
+
     shellAliases = {
       # Navigating the filesystem
       ll = "ls -vlAh --color=auto --group-directories-first";
@@ -35,5 +36,64 @@
       untranslated="echo -n \"<target.*>[^\\s]*\\.[^\\s]*</target>\" | cclip | echo \"Copied to clipboard!\"";
       untranslated-csv="echo -n \";(\\\"|)[^\\s]*\\.[^\\s]*\" | cclip | echo \"Copied to clipboard!\"";
     };
+
+    initExtra = ''
+      #Regular text color
+      BLACK='\[\e[0;30m\]'
+      #Bold text color
+      BBLACK='\[\e[1;30m\]'
+      #background color
+      BGBLACK='\[\e[40m\]'
+      RED='\[\e[0;31m\]'
+      BRED='\[\e[1;31m\]'
+      BGRED='\[\e[41m\]'
+      GREEN='\[\e[0;32m\]'
+      BGREEN='\[\e[1;32m\]'
+      BGGREEN='\[\e[1;32m\]'
+      YELLOW='\[\e[0;33m\]'
+      BYELLOW='\[\e[1;33m\]'
+      BGYELLOW='\[\e[1;33m\]'
+      BLUE='\[\e[0;34m\]'
+      BBLUE='\[\e[1;34m\]'
+      BGBLUE='\[\e[1;34m\]'
+      MAGENTA='\[\e[0;35m\]'
+      BMAGENTA='\[\e[1;35m\]'
+      BGMAGENTA='\[\e[1;35m\]'
+      CYAN='\[\e[0;36m\]'
+      BCYAN='\[\e[1;36m\]'
+      BGCYAN='\[\e[1;36m\]'
+      WHITE='\[\e[0;37m\]'
+      BWHITE='\[\e[1;37m\]'
+      BGWHITE='\[\e[1;37m\]'
+
+      PROMPT_COMMAND=smile_prompt
+
+      function smile_prompt
+      {
+        if [ "$?" -eq "0" ]
+        then
+        #smiley
+        SC="''\${GREEN}:)"
+        else
+        #frowney
+        SC="''\${RED}:("
+        fi
+        if [ $UID -eq 0 ]
+        then
+        #root user color
+        UC="''\${RED}"
+        else
+        #normal user color
+        UC="''\${BYELLOW}"
+        fi
+        #hostname color
+        HC="''\${BYELLOW}"
+        #regular color
+        RC="''\${BWHITE}"
+        #default color
+        DF='\[\e[0m\]'
+        PS1="[''\${UC}\u''\${RC}@''\${HC}\h ''\${RC}\w''\${DF}] ''\${SC}''\${DF} "
+      }
+    '';
   };
 }
