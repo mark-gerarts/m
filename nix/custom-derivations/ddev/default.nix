fd0521758e5de3a0e36bdbc7665b496336ed9e37
Mark Gerarts <mark.gerarts@gmail.com>
2021-06-05 09:49:53 +0200

    Upgrade ddev

--- nix/custom-derivations/ddev/default.nix
+++ nix/custom-derivations/ddev/default.nix
@@ -4,8 +4,8 @@ with pkgs;
 
 runCommand "ddev" {
     src = fetchurl {
-        url = "https://github.com/drud/ddev/releases/download/v1.16.4/ddev_linux-amd64.v1.16.4.tar.gz";
-        sha256 = "1qyr45iiyxf2775wg94imzzr52c3w1sbn6lgvd552hdcyfqh7xi8";
+        url = "https://github.com/drud/ddev/releases/download/v1.16.7/ddev_linux-amd64.v1.16.7.tar.gz";
+        sha256 = "1ah0rd0s48h4f6avfsbrgn917w91f9a46h0j186j82s0cw5p5vvm";
     };
 } ''
     mkdir -p $out/bin
