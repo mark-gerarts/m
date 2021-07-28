8103cbd9e12c7341bcb82cfa9fdb8b5f30ec467b
Mark Gerarts <mark.gerarts@gmail.com>
2021-07-28 09:32:29 +0200

    Update ddev

--- nix/custom-derivations/ddev/default.nix
+++ nix/custom-derivations/ddev/default.nix
@@ -4,8 +4,8 @@ with pkgs;
 
 runCommand "ddev" {
     src = fetchurl {
-        url = "https://github.com/drud/ddev/releases/download/v1.16.7/ddev_linux-amd64.v1.16.7.tar.gz";
-        sha256 = "1ah0rd0s48h4f6avfsbrgn917w91f9a46h0j186j82s0cw5p5vvm";
+        url = "https://github.com/drud/ddev/releases/download/v1.17.7/ddev_linux-amd64.v1.17.7.tar.gz";
+        sha256 = "1rdpiynisf1vvlplbdn9ybjya964a2hwyf8zfy3gpn8rrbwzsqq5";
     };
 } ''
     mkdir -p $out/bin
