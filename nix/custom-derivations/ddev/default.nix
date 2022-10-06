b6c9a23c80819eab1195a24a2dda8c7dc2988424
Mark Gerarts <mark.gerarts@gmail.com>
2022-10-06 16:31:12 +0200

    Update ddev

--- nix/custom-derivations/ddev/default.nix
+++ nix/custom-derivations/ddev/default.nix
@@ -4,8 +4,8 @@ with pkgs;
 
 runCommand "ddev" {
     src = fetchurl {
-        url = "https://github.com/drud/ddev/releases/download/v1.19.3/ddev_linux-amd64.v1.19.3.tar.gz";
-        sha256 = "1zsfycfbh1a3g09x7i71ha0skkcd4nnlxvabjmrzy55q780p29m2";
+        url = "https://github.com/drud/ddev/releases/download/v1.21.1/ddev_linux-amd64.v1.21.1.tar.gz";
+        sha256 = "1a3faax9gncgr1jl0yg8qs1vil0rx07pkwycyf8xk9vvzi724v19";
     };
 } ''
     mkdir -p $out/bin
