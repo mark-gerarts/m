be2b32532a76597b0c12a9112beefb14334980a0
Mark Gerarts <mark.gerarts@gmail.com>
2022-10-10 15:42:56 +0200

    Let Docker use smaller network ranges

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -6,6 +6,24 @@
       enable = true;
       enableOnBoot = true;
 
+      # By default, docker uses oversized network ranges, resulting in less
+      # total available networks. We override the default address pools to use
+      # smaller ranges, thus creating more possible networks.
+      #
+      # @see https://straz.to/2021-09-08-docker-address-pools/
+      daemon.settings = {
+        default-address-pools = [
+          {
+            base = "172.17.0.0/12";
+            size = 20;
+          }
+          {
+            base = "192.168.0.0/16";
+            size = 24;
+          }
+        ];
+      };
+
       # The Docker daemon by default alters iptables to allow outside traffic.
       # The Docker documentation states that it is probably not a good idea to
       # disable this, so if things break this might be a good place to start
