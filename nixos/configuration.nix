a14c1389970daf56ccc9f570ce240c8052ca6089
Mark Gerarts <mark.gerarts@gmail.com>
2025-04-11 21:25:13 +0200

    Move every nixos thingy to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,27 +7,31 @@
       ./device-specific-configuration.nix
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.useOSProber = true;
+  ##
+  ## Config
+  ##
 
-  networking.networkmanager.enable = true;
+  # Forgive me for my sins, RMS.
+  nixpkgs.config.allowUnfree = true;
 
-  time.timeZone = "Europe/Brussels";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  services.xserver = {
+  boot.loader.grub = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    useOSProber = true;
   };
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  time = {
+    timeZone = "Europe/Brussels";
+    hardwareClockInLocalTime = true; # To please Windows...
   };
 
   services.printing.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # For quick hostfile changes (lost on rebuild)
+  environment.etc.hosts.mode = "0644";
 
   # Sound.
   hardware.pulseaudio.enable = false;
@@ -42,14 +46,110 @@
   users.users.mark = {
     isNormalUser = true;
     description = "Mark";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [];
   };
+  nix.settings.trusted-users = [ "root" "mark" ];
 
-  programs.firefox.enable = true;
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 30d";
+  };
 
-  # Forgive me for my sins, RMS.
-  nixpkgs.config.allowUnfree = true;
+  services.journald.extraConfig = ''
+    SystemMaxUse=50M
+  '';
+
+  # Powering off takes 90s to finish because containerd doesn't get killed
+  # properly. This is a workaround. See
+  # https://github.com/containerd/containerd/issues/386#issuecomment-304837687
+  systemd.services.docker.serviceConfig.KillMode = "mixed";
+
+  # Also, 90s is overkill anyways.
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+  '';
+
+  system.autoUpgrade = {
+    enable = true;
+    persistent = true;
+    dates = "daily";
+  };
+
+  ##
+  ## Gnome
+  ##
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    # TODO: I do the following steps manually for now, can we set this up
+    # automatically?
+    # https://github.com/qwerty-fr/qwerty-fr/issues/49#issuecomment-1405254634
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+    cheese
+    gnome-music
+    epiphany
+    geary
+    totem
+    tali
+    iagno
+    hitori
+    atomix
+    yelp
+    seahorse
+    gnome-contacts
+    gnome-initial-setup
+  ]);
+
+  ##
+  ## Virtualisation
+  ##
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      enableOnBoot = true;
+
+      # By default, docker uses oversized network ranges, resulting in less
+      # total available networks. We override the default address pools to use
+      # smaller ranges, thus creating more possible networks.
+      #
+      # @see https://straz.to/2021-09-08-docker-address-pools/
+      daemon.settings = {
+        default-address-pools = [
+          {
+            base = "172.17.0.0/12";
+            size = 20;
+          }
+          {
+            base = "192.168.0.0/16";
+            size = 24;
+          }
+        ];
+      };
+    };
+  };
+
+  ##
+  ## Packages
+  ##
+
+  programs = {
+    firefox.enable = true;
+    gnupg.agent.enable = true;
+  };
+
+  services.flatpak.enable = true;
 
   environment.systemPackages = with pkgs; [
     # System essentials
@@ -60,9 +160,14 @@
 
     # Dev
     vscode
-  ];
+    gnome-terminal
 
-  programs.gnupg.agent.enable = true;
+    # Gnome extensions
+    gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.dash-to-dock
+  ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -71,5 +176,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
