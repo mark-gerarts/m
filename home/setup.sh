f2ff426192de77886d3d08e72e510fdc7eec477e
Mark Gerarts <mark.gerarts@gmail.com>
2025-12-29 14:52:07 +0100

    Fix bash script dir command

--- home/setup.sh
+++ home/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 create_symlinks() {
     echo "Symlinking dotfiles..."
